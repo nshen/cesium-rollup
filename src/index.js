ac3074bbd03ee48bd62e6cfbf01a79c0524fb39e
Xin.Chen <xin.chen@percent.cn>
2020-12-02 16:03:27 +0800

    make buindle.js defer

--- src/index.js
+++ src/index.js
@@ -1,21 +1,18 @@
 import { Cesium3DTileset, createWorldTerrain, IonResource, Viewer } from 'Cesium';
 const CESIUM_BASE_URL = '';
-window.onload = () => {
 
-    const viewer = new Viewer('cesiumContainer', {
-        // terrainProvider: createWorldTerrain()
-        // scene3DOnly: true,
-        // geocoder: false,
-        // selectionIndicator: false,
-        // fullscreenButton: false,
-        // animation: false,
-        // homeButton: false,
-        // navigationHelpButton: false,
-        // timeline: false
-        // baseLayerPicker: false, //关掉才能换底图
+const viewer = new Viewer('cesiumContainer', {
+    // terrainProvider: createWorldTerrain()
+    // scene3DOnly: true,
+    // geocoder: false,
+    // selectionIndicator: false,
+    // fullscreenButton: false,
+    // animation: false,
+    // homeButton: false,
+    // navigationHelpButton: false,
+    // timeline: false
+    // baseLayerPicker: false, //关掉才能换底图
 
 
-    });
-    console.log(viewer.imageryLayers);
-
-};
+});
+console.log(viewer.imageryLayers);
